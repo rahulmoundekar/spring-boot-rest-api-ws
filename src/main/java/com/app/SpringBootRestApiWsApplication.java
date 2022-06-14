0c8d36164926d0adf8ea5cbd898ea5e126348faf
Rahul Moundekar <rahulmoundekar44@gmail.com>
2022-06-14 20:53:46 +0530

    Update SpringBootRestApiWsApplication.java

--- src/main/java/com/app/SpringBootRestApiWsApplication.java
+++ src/main/java/com/app/SpringBootRestApiWsApplication.java
@@ -8,6 +8,7 @@ public class SpringBootRestApiWsApplication {
 
 	public static void main(String[] args) {
 		System.out.println("Spring boot web service example");
+		System.out.println("Spring boot web service with git");
 		SpringApplication.run(SpringBootRestApiWsApplication.class, args);
 	}
 
