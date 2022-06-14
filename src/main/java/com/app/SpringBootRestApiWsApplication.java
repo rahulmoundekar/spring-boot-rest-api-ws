55151097a799cc22b4222ce3a9888c2835d3dfa7
Rahul Moundekar <rahulmoundekar44@gmail.com>
2022-06-14 20:51:30 +0530

    Spring boot web service example

--- src/main/java/com/app/SpringBootRestApiWsApplication.java
+++ src/main/java/com/app/SpringBootRestApiWsApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootRestApiWsApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Spring boot web service example");
 		SpringApplication.run(SpringBootRestApiWsApplication.class, args);
 	}
 
