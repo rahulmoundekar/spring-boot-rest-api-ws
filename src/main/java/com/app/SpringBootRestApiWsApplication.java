ab007569e6cc526ffd8994294868f470ed050145
Rahul Moundekar <rahulmoundekar44@gmail.com>
2022-06-14 20:11:18 +0530

    spring boot web service application

---
+++ src/main/java/com/app/SpringBootRestApiWsApplication.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestApiWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestApiWsApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/app/exception/CustomerNotFoundException.java
@@ -0,0 +1,18 @@
+package com.app.exception;
+
+public class CustomerNotFoundException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public CustomerNotFoundException() {
+		super();
+	}
+
+	public CustomerNotFoundException(String msg) {
+		super(msg);
+	}
+
+}
